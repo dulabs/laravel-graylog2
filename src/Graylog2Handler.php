21ecff574cc5686a40b4a7db435087daf2fa275d
Dulabs <dev@dulas.com>
2016-12-10 21:26:30 +0000

    fix newline

--- src/Graylog2Handler.php
+++ src/Graylog2Handler.php
@@ -1,37 +1,21 @@
 <?php namespace Dulabs\Graylog2;
 
-
-
 use Monolog\Handler\AbstractHandler;
 
-
-
 class Graylog2Handler extends AbstractHandler
-
 {
-
     /**
-
      * Handle the log record.
-
      *
-
      * @param array $record
-
      *
-
      * @return bool
-
      */
 
     public function handle(array $record = [])
-
     {
-
         // Do something with the $record array
-
         try {
-
             $graylog2 = app('graylog2');
 
             $message = $record['message'];
@@ -39,13 +23,8 @@ class Graylog2Handler extends AbstractHandler
             $loglevel = strtolower($record['level_name']);
 
             $graylog2->{$loglevel}($message);
-
         } catch (Exception $e) {
-
             Log::error("cannot log error to graylog");
-
         }
-
     }
-
 }
